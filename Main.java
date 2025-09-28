458b2b57ad03114782dd2a1407d3956ea280e6c7
Rajendra P M <rajendrapm03@gmail.com>
2025-09-28 22:25:15 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello! This is a simple Java application for CI/CD demo.");
+        System.out.println("Hello! This is a simple Java application for CI/CD .");
     }
 }
