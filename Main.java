03e2d873d8d8a6f38bdc003dc2372f3aa6fb865b
Rajendra P M <rajendrapm03@gmail.com>
2025-10-06 22:35:07 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,33 +1,33 @@
-// public class Main {
-//     public static void main(String[] args) {
-//         System.out.println("Hello! This is a simple Java application for CI/CD .");
-//     }
-// }
-
-import com.sun.net.httpserver.HttpServer;
-import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpExchange;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.InetSocketAddress;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
-        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
-        server.createContext("/", new MyHandler());
-        server.setExecutor(null); // default executor
-        server.start();
-        System.out.println("Server started at http://0.0.0.0:8080");
-    }
-
-    static class MyHandler implements HttpHandler {
-        public void handle(HttpExchange t) throws IOException {
-            String response = "<h1>Hello! This is a simple Java web app for CI/CD</h1>";
-            t.getResponseHeaders().add("Content-Type", "text/html");
-            t.sendResponseHeaders(200, response.getBytes().length);
-            OutputStream os = t.getResponseBody();
-            os.write(response.getBytes());
-            os.close();
-        }
+    public static void main(String[] args) {
+        System.out.println("Hello! This is a simple Java application for CI/CD .");
     }
 }
+
+// import com.sun.net.httpserver.HttpServer;
+// import com.sun.net.httpserver.HttpHandler;
+// import com.sun.net.httpserver.HttpExchange;
+// import java.io.IOException;
+// import java.io.OutputStream;
+// import java.net.InetSocketAddress;
+
+// public class Main {
+//     public static void main(String[] args) throws IOException {
+//         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+//         server.createContext("/", new MyHandler());
+//         server.setExecutor(null); // default executor
+//         server.start();
+//         System.out.println("Server started at http://0.0.0.0:8080");
+//     }
+
+//     static class MyHandler implements HttpHandler {
+//         public void handle(HttpExchange t) throws IOException {
+//             String response = "<h1>Hello! This is a simple Java web app for CI/CD</h1>";
+//             t.getResponseHeaders().add("Content-Type", "text/html");
+//             t.sendResponseHeaders(200, response.getBytes().length);
+//             OutputStream os = t.getResponseBody();
+//             os.write(response.getBytes());
+//             os.close();
+//         }
+//     }
+// }
