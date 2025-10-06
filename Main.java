a3e45f7104d83ab5bf9b0b02c2da852c2b163a86
Rajendra P M <rajendrapm03@gmail.com>
2025-10-06 23:04:22 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,26 @@
+// public class Main {
+//     public static void main(String[] args) {
+//         System.out.println("Hello! This is a simple Java application for CI/CD .");
+//     }
+// }
+
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import com.sun.net.httpserver.HttpServer;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello! This is a simple Java application for CI/CD .");
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/", exchange -> {
+            String response = "Hello Rajendra! Your Java App is running on EC2 🚀";
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        });
+        System.out.println("Server started on port 8080...");
+        server.start();
     }
 }
-
-
