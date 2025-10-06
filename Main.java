697f92015a562f3a4cac47ecec1f2727dd4e12f3
Rajendra P M <rajendrapm03@gmail.com>
2025-10-06 23:32:40 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,22 +5,31 @@
 // }
 
 
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
-import com.sun.net.httpserver.HttpServer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
-        server.createContext("/", exchange -> {
-            String response = "Hello Rajendra! Your Java App is running on EC2 🚀";
-            exchange.sendResponseHeaders(200, response.length());
-            OutputStream os = exchange.getResponseBody();
+        server.createContext("/", new MyHandler());
+        server.setExecutor(null); // default executor
+        server.start();
+        System.out.println("Server started at http://0.0.0.0:8080");
+    }
+
+    static class MyHandler implements HttpHandler {
+        public void handle(HttpExchange t) throws IOException {
+            String response = "<h1>Hello! This is a simple Java web app for CI/CD</h1>";
+            t.getResponseHeaders().add("Content-Type", "text/html");
+            t.sendResponseHeaders(200, response.getBytes().length);
+            OutputStream os = t.getResponseBody();
             os.write(response.getBytes());
             os.close();
-        });
-        System.out.println("Server started on port 8080...");
-        server.start();
+        }
     }
 }
+
